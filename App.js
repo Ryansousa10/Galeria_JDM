eec64a45672e668dddf52c17cb0a84337cb3e8d9
Ryan Sousa <Ryan.sousa4000@gmail.com>
2022-06-01 00:17:37 -0300

    Dropdown

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 
-// import styles from "./App.module.css"
+import styles from "./App.module.css"
 
 
 import Header from './Components/Header/Header';
 import ContainerCard from './Components/ContainerCard/ContainerCard';
+import Dropdown from './Components/Elements/Dropdown/Dropdown';
+
+import images from "./Components/Jsons/Images.json";
 
 
 const App = () => {
+                      // Dropdown aqui
+  const ddItems = [
+    {
+      id: 1,
+      title: "Mazda",
+      active: true
+    },
+    {
+      id: 2,
+      title: "Nissan",
+      active: false
+    },
+    {
+      id: 3,
+      title: "Toyota",
+      active: false
+    },
+    {
+      id: 4,
+      title: "Mitsubishi",
+      active: false
+    },
+    {
+      id: 5,
+      title: "Honda",
+      active: false
+    },
+    {
+      id: 6,
+      title: "Todos",
+      active: false
+    }
+  ]
+
+  const [categoryImage, setCategoryImage] = useState(images.categories.all)
+
+  const takeDdTitle = (ddTitle) => {
+    setCategoryImage(() => {
+      let categoryChoose = Object.keys(images.categories).filter(item => {
+        const titleSplited = ddTitle.toLowerCase().split(" ")[0]
+        return item.toLowerCase().includes(titleSplited)
+      })
+      return [ ...images.categories[categoryChoose] ]
+    })
+  }
 
   return (
     <>
         <Header />
         <div className="flex justify-content-center" style={{ marginTop: "50px", padding: '50px' }}>
 
-          <ContainerCard />
+          <ContainerCard>
+              <div className={`${styles["gallery-setting"]} flex justify-content-between align-items-center`}>
+                
+                <Dropdown title="Todos" items={ddItems} liftingDdTextUp={takeDdTitle} />
+              </div>
+               
+          </ContainerCard>
           
         </div>
     </>
---
+++ Components/Elements/Button/Button.jsx
@@ -0,0 +1,22 @@
+import styles from "./Button.module.css"
+
+// react package
+import PropTypes from 'prop-types'
+
+const Button = ({ children, theme, onClick, className }) => (
+    <button
+        className={`${styles.button} ${styles[theme]} ${className}`}
+        onClick={onClick}
+    >
+        {children}
+    </button>
+)
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    theme: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    className: PropTypes.string
+}
+
+export default Button
